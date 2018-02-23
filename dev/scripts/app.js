3da10aa299b820260d3e19e8c3d5713768bf9272
Jason Du <jayd74@gmail.com>
2018-02-22 21:54:30 -0500

    created jordan.json and adding aj 1 to 11

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Jordan from '../../jordan.json'
 
+
+console.log(Jordan)
 class App extends React.Component {
     render() {
       return (
         <div>
-          Hello
+          Jordan
         </div>
       )
     }
